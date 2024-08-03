0904f565ca60a71be4c356df533be47af9593603
Jai Sharma <jaisharma687@outlook.com>
2024-08-04 01:47:57 +0530

    Fixed Menu not showing issue in vertical orientation

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,11 +13,16 @@ export const Header = () => {
     <header className="py-8 xl:py-12 text-white">
         <div className="container mx-auto flex justify-between items-center">
             {/* logo */}
-            <Link href="/">
-              <h1 className="text-4xl font-semibold">
-                  <span className="hover:text-red">BlocDoc</span><span className="text-accent">.</span>
-              </h1>
-            </Link>
+            <div className="flex flex-col gap-4">
+              <Link href="/">
+                <h1 className="text-4xl font-semibold">
+                    <span className="hover:text-red">BlocDoc</span><span className="text-accent">.</span>
+                </h1>
+              </Link>
+              <div className="xl:hidden flex gap-8 items-center">
+                <ConnectWallet account={account} setAccount={setAccount} />
+              </div>
+            </div>
             {/* desktop navbar & connect wallet */}
             <div className="hidden xl:flex items-center gap-8">
               <Nav />
@@ -25,7 +30,6 @@ export const Header = () => {
             </div>
             {/* mobile navbar */}
             <div className="xl:hidden flex gap-8 items-center">
-              <ConnectWallet account={account} setAccount={setAccount} />
               <MobileNav />
             </div>
         </div>
