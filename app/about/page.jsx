19f117fb1e0cd404da42fba96aaf3be040cc3b9b
siddhant22bce0682 <156163973+Siddhantbg@users.noreply.github.com>
2024-08-11 01:25:59 +0530

    update about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div class="flex flex-wrap -m-4">
       <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
         <div class="h-full text-center">
-          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/>
+          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/"/>
           <p class="leading-relaxed">CSE CORE</p>
           <span class="inline-block h-1 w-10 rounded bg-orange-700 mt-6 mb-4"></span>
           <h2 class="text-white font-medium title-font tracking-wider text-sm">JAI SHARMA</h2>
@@ -16,7 +16,7 @@ const About = () => {
       </div>
       <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
         <div class="h-full text-center">
-          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/300x300"/>
+          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/"/>
           <p class="leading-relaxed">CSE CORE</p>
           <span class="inline-block h-1 w-10 rounded bg-orange-700 mt-6 mb-4"></span>
           <h2 class="text-white font-medium title-font tracking-wider text-sm">SIDDHANT BHAGAT</h2>
@@ -25,7 +25,7 @@ const About = () => {
       </div>
       <div class="lg:w-1/3 lg:mb-0 p-4">
         <div class="h-full text-center">
-          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/305x305"/>
+          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/"/>
           <p class="leading-relaxed">CSE BLOCKCAHIN</p>
           <span class="inline-block h-1 w-10 rounded bg-orange-700 mt-6 mb-4"></span>
           <h2 class="text-white font-medium title-font tracking-wider text-sm">SHREYASH NAIK</h2>
