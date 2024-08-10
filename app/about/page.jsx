b75d51c88cd2eb08b9d28148fd0c5e23eb01052c
siddhant22bce0682 <156163973+Siddhantbg@users.noreply.github.com>
2024-08-11 01:24:54 +0530

    added about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,41 @@
+import React from "react"
 const About = () => {
   return (
-    <div>About Page</div>
+    <>
+    <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+        <div class="h-full text-center">
+          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/>
+          <p class="leading-relaxed">CSE CORE</p>
+          <span class="inline-block h-1 w-10 rounded bg-orange-700 mt-6 mb-4"></span>
+          <h2 class="text-white font-medium title-font tracking-wider text-sm">JAI SHARMA</h2>
+          <p class="text-gray-500">22BCE3969</p>
+        </div>
+      </div>
+      <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+        <div class="h-full text-center">
+          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/300x300"/>
+          <p class="leading-relaxed">CSE CORE</p>
+          <span class="inline-block h-1 w-10 rounded bg-orange-700 mt-6 mb-4"></span>
+          <h2 class="text-white font-medium title-font tracking-wider text-sm">SIDDHANT BHAGAT</h2>
+          <p class="text-gray-500">22BCE0682</p>
+        </div>
+      </div>
+      <div class="lg:w-1/3 lg:mb-0 p-4">
+        <div class="h-full text-center">
+          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/305x305"/>
+          <p class="leading-relaxed">CSE BLOCKCAHIN</p>
+          <span class="inline-block h-1 w-10 rounded bg-orange-700 mt-6 mb-4"></span>
+          <h2 class="text-white font-medium title-font tracking-wider text-sm">SHREYASH NAIK</h2>
+          <p class="text-gray-500">22BKT0053</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </>
   )
 }
 export default About
